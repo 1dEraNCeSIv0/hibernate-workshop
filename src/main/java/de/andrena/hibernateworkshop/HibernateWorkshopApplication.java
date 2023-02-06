90ca5ea6ce9236f159c661a3a3c181210964d5e8
Ruben Gehring <rgehring@andrena.de>
2023-02-06 10:36:14 +0100

    Initialize via Sprint Starter

---
+++ src/main/java/de/andrena/hibernateworkshop/HibernateWorkshopApplication.java
@@ -0,0 +1,13 @@
+package de.andrena.hibernateworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/andrena/hibernateworkshop/HibernateWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package de.andrena.hibernateworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
