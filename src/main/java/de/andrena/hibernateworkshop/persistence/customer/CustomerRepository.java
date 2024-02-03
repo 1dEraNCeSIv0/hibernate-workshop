122c4e118e5ce3a962006193c3bf6b1b2f4c20df
Ruben Gehring <ruben.gehring@andrena.de>
2024-02-03 10:40:24 +0100

    Updated Java, Spring Boot, Gradle

--- src/main/java/de/andrena/hibernateworkshop/persistence/customer/CustomerRepository.java
+++ src/main/java/de/andrena/hibernateworkshop/persistence/customer/CustomerRepository.java
@@ -3,26 +3,24 @@ package de.andrena.hibernateworkshop.persistence.customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 
-    @Query("""
-            SELECT c
-            FROM Customer c
-            JOIN c.address
-            JOIN c.checkedOutBooks
-            JOIN c.favoriteAuthors
-            WHERE c.name = :name
-            """)
-    Optional<Customer> findByName(String name);
+	@Query("""
+		SELECT c
+		FROM Customer c
+		JOIN c.address
+		JOIN c.checkedOutBooks
+		JOIN c.favoriteAuthors
+		WHERE c.name = :name
+		""")
+	Optional<Customer> findByName(String name);
 
-    @Query("SELECT c FROM Customer c")
-//    @EntityGraph(attributePaths = ...)
-    List<Customer> findAllUsingEntityGraph();
+	@Query("SELECT c FROM Customer c")
+		//    @EntityGraph(attributePaths = ...)
+	List<Customer> findAllUsingEntityGraph();
 
-    <T> List<T> findAllProjectionsBy(Class<T> projectionClass);
+	<T> List<T> findAllProjectionsBy(Class<T> projectionClass);
 
 }
